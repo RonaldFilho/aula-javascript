26e740de1c279c472471cbce231ad050b85e5882
RonaldFilho <ronaldfilhocrente@gmail.com>
2021-11-01 12:06:21 -0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,16 @@
+var nome = "Ronald Filho";
+//var idade = 21; 
+//var idade2 = 10;
+var n1 = 3;
+var n2 = 5;
+var frase = "Japão é o melhor time do mundo.";
+//alert(nome + " tem " + idade + " anos.");
+//alert(idade + idade2);
+
+console.log(nome);
+//console.log(idade + idade2);
+console.log(n1 * n2);
+console.log(frase.replace("Japão", "Brasil"));
+//console.log(frase.toUpperCase());
+//console.log(frase.toLowerCase()); 
+//alert(frase.replace("Japão", "Brasil"));
