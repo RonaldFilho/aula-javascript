2bdb70fdab485cc312c8347f4c02546453409451
RonaldFilho <ronaldfilhocrente@gmail.com>
2021-11-02 15:04:07 -0300

    array e dicionário

--- js/main.js
+++ js/main.js
@@ -1,16 +1,41 @@
-var nome = "Ronald Filho";
-//var idade = 21; 
-//var idade2 = 10;
+/*var nome = "Ronald Filho";
+var idade = 21; 
+var idade2 = 10;
 var n1 = 3;
 var n2 = 5;
 var frase = "Japão é o melhor time do mundo.";
-//alert(nome + " tem " + idade + " anos.");
-//alert(idade + idade2);
+alert(nome + " tem " + idade + " anos.");
+alert(idade + idade2);*/
 
-console.log(nome);
-//console.log(idade + idade2);
+/*console.log(nome);
+console.log(idade + idade2);
 console.log(n1 * n2);
 console.log(frase.replace("Japão", "Brasil"));
-//console.log(frase.toUpperCase());
-//console.log(frase.toLowerCase()); 
-//alert(frase.replace("Japão", "Brasil"));
+console.log(frase.toUpperCase());
+console.log(frase.toLowerCase()); 
+alert(frase.replace("Japão", "Brasil"));*/
+
+
+//Array
+/*var lista = ["maçã","pêra","laranja"];
+lista.push("uva"); //*push - adiciona elementos na lista* usando o nome do elemento
+lista.pop([1]); //*pop - remove elementos da lista* usando o índice 
+console.log(lista);
+console.log(lista.length); //tamanho da lista
+console.log(lista.reverse()); //lista os elementos na ordem reversa
+console.log(lista[0]); //lista o elemento indicado
+console.log(lista.toString()[0]); //*toString - transforma lista em string* lista o elemento 0 da string
+console.log(lista.join(" - ")); //*join - transforma em string e separa os elementos com o valor definido em ()
+alert(lista[1]);*/
+
+//Dicionário
+/*var fruta = {nome:"maçã", cor:"vermelha"};
+console.log(fruta.nome);
+alert(fruta.cor);*/
+
+//Lista de Dicionário
+/*var frutas = [{nome:"maçã", cor:"vermelha"}, {nome:"uva", cor:"roxa"}];
+console.log(frutas);
+alert(frutas[1].nome);*/
+
+//Condicionais
